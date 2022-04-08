f6209e2661b9e3b58845de813d7d5ea4a20ec988
Joan Sebastian Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-04-08 14:22:36 -0500

    Feat: Added account and screen login

---
+++ credentials.py
@@ -0,0 +1,6 @@
+
+def getUsername()->str:
+    return 'scrapingweb08@gmail.com'
+
+def getPassword()->str:
+    return 'Chelsea123!'
--- web_scraping.py
+++ web_scraping.py
@@ -1,19 +1,27 @@
+#Libraries
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time, urllib.request
 import requests
 
+# Credentials
+from credentials import getPassword, getUsername
+
+#Chrome Web Driver
 PATH = "C:\\Users\\SebastianCB-dev\\Downloads\\chromedriver_win32\\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
+# Open Instagram URL
 driver.get("https://www.instagram.com/")
 
-#login
+# Screen Login Instagram
 time.sleep(5)
 username = driver.find_element_by_css_selector("input[name='username']")
 password = driver.find_element_by_css_selector("input[name='password']")
 username.clear()
 password.clear()
-username.send_keys("Input User")
-password.send_keys("password")
-time.sleep(1000)
+username.send_keys(getUsername())
+password.send_keys(getPassword())
+time.sleep(5)
+login = driver.find_element_by_css_selector("button[type='submit']").click()
+time.sleep(20)
