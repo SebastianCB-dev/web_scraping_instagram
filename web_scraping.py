1f7191e288e0e13f3d5660f9fa8997e32aec4e93
Joan Sebastian Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-04-08 17:35:21 -0500

    Feat: Added web scraping to main page

--- web_scraping.py
+++ web_scraping.py
@@ -16,12 +16,22 @@ driver.get("https://www.instagram.com/")
 
 # Screen Login Instagram
 time.sleep(5)
-username = driver.find_element_by_css_selector("input[name='username']")
-password = driver.find_element_by_css_selector("input[name='password']")
-username.clear()
-password.clear()
-username.send_keys(getUsername())
-password.send_keys(getPassword())
+username_input = driver.find_element_by_css_selector("input[name='username']")
+password_input = driver.find_element_by_css_selector("input[name='password']")
+username_input.clear()
+password_input.clear()
+username_input.send_keys(getUsername())
+password_input.send_keys(getPassword())
 time.sleep(5)
 login = driver.find_element_by_css_selector("button[type='submit']").click()
 time.sleep(20)
+
+# Screen Save your login info
+login_save_info_button_not_now = driver.find_element_by_xpath("//button[contains(text(), 'Not now')]").click()
+time.sleep(10)
+
+# Screen Turn On Notifications
+turn_on_notificacion_not_not_button = driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
+time.sleep(10)
+
+# Search person 
